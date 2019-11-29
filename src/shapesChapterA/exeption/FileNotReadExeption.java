4700e1cc8461c0c759a1a24295c914d97c73743e
VladimirLiukhtsenka <liukhtenko.v@mail.ru>
2019-11-30 00:37:28 +0300

    +

---
+++ src/shapesChapterA/exeption/FileNotReadExeption.java
@@ -0,0 +1,19 @@
+package shapesChapterA.exeption;
+
+public class FileNotReadExeption extends Exception {
+    public FileNotReadExeption() {
+        super();
+    }
+
+    public FileNotReadExeption(String message) {
+        super(message);
+    }
+
+    public FileNotReadExeption(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FileNotReadExeption(Throwable cause) {
+        super(cause);
+    }
+}

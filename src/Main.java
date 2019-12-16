0a94c24c18b82e9cd64ccb32e3e2a418ff08f2c8
VladimirLiukhtsenka <liukhtenko.v@mail.ru>
2019-12-17 00:49:02 +0300

    shape Task1

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import com.liukhtenko.taskShape.entity.Point;
-
-public class Main {
-    public static void main(String[] args) {
-        Point point = new Point(1,2,3);
-        System.out.println(point);
-    }
-}
